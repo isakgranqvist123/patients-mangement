57342d476816f71a87b2adaf2dd684673be3ffc0
isakgranqvist2021 <isakwebdev@gmail.com>
2025-06-27 15:50:38 +0200

    test

--- frontend/src/modules/edit-patient.tsx
+++ frontend/src/modules/edit-patient.tsx
@@ -23,8 +23,6 @@ import {
   DialogTitle,
 } from '../components/ui/dialog';
 
-console.log('Deploy');
-
 const editPatientSchema = z.object({
   firstName: z.string().min(1, 'First name is required'),
   lastName: z.string().min(1, 'Last name is required'),
